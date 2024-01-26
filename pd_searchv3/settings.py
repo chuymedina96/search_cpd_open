88e80e1c6cbdd57193250093b35fc3f0b9b06a19
Jesus Medina <87674772+chuymedina1396@users.noreply.github.com>
2024-01-25 18:31:22 -0600

    more

--- pd_searchv3/settings.py
+++ pd_searchv3/settings.py
@@ -127,15 +127,15 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL= None
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage"
-    },
-}
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "storages.backends.s3.S3Storage"
+#     },
+# }
 
-STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
+# STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
